1e74cc93caf9323be65e14e882fab6ecb3af48d1
Akswarum <murawskaga@gmail.com>
2017-10-21 16:43:53 +0200

    zadanie 2 i 3

--- js/app.js
+++ js/app.js
@@ -1,17 +1,51 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', function() {
 
+    
+    // zadanie1
     var menuElement = document.querySelector('.for-dropdown');
     var dropdownList = document.querySelector('.for-dropdown .dropdown');
     
     menuElement:addEventListener('mouseover', function(){
         dropdownList.style.display = 'block';
-        
-    
     });
     
-      menuElement:addEventListener('mouseout', function(){
+    menuElement:addEventListener('mouseout', function(){
           dropdownList.style.display = 'none';
-      });
-    
     });
+    
+ 
+    
+    //zadanie2
+    var buttons = document.querySelectorAll('.btn.read-more');
+    
+    function showHide() {
+        var textArea = this.previousElementSibling;
+        
+      if(textArea.style.display === 'none' || textArea.style.display === ' '){
+       textArea.style.display = 'block';   
+        this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';      
+      } else {
+          textArea.style.display = 'none';
+          this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>';
+      } 
+    }
+   // buttons[0].addEventListener('click', showHide);
+    
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', showHide);
+    }
+    
+    //zadanie3
+    
+    var navigation = document.querySelector('nav.navbar');
+    
+    window.addEventListener('scroll', function() {
+        navigation.style.borderBottom = '1px solid #7f7f7f';
+        
+    });
+    
+    
+    
+});
+   
